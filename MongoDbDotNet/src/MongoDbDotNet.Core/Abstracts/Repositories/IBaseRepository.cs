e15be700e2e38f56041a261105ab19ae0b8c809d
Hadi <se.hadireazee@gmail.com>
2023-08-27 11:52:09 +0200

    feat: add base repo interface

---
+++ MongoDbDotNet/src/MongoDbDotNet.Core/Abstracts/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+
+namespace MongoDbDotNet.Core.Abstracts.Repositories;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    Task<List<TEntity>> GetAllAsync();
+    Task<TEntity?> GetByIdAsync(string id);
+    Task<TEntity> GetSingleAsync(FilterDefinition<TEntity> predicate);
+    Task InsertAsync(TEntity entity);
+    Task UpdateAsync(string id, TEntity entity);
+    Task DeleteAsync(string id);
+}
