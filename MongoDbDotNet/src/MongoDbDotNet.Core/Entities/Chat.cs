00bdc3867c9247ebf927e40d4949ec15abed3a42
Hadi <se.hadireazee@gmail.com>
2023-08-27 12:02:03 +0200

    feat: add sample entity models

---
+++ MongoDbDotNet/src/MongoDbDotNet.Core/Entities/Chat.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MongoDbDotNet.Core.Entities;
+
+public class Chat
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    [BsonElement("message")]
+    public Message Message { get; set; }
+    
+    [BsonElement("version")]
+    public string Version { get; set; }
+}
+
+public class Message
+{
+    [BsonElement("topic")]
+    public string Topic { get; set; }
+    [BsonElement("user")]
+    public string User { get; set; }
+    [BsonElement("created_date")]
+    public string CreatedDate { get; set; }
+    [BsonElement("created_timestamp")]
+    public double CreatedTimestamp { get; set; }
+}
---
+++ MongoDbDotNet/src/MongoDbDotNet.Core/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MongoDbDotNet.Core.Entities;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    [BsonElement("email")]
+    public string Email { get; set; }
+    [BsonElement("role")]
+    public string Role { get; set; }
+}
