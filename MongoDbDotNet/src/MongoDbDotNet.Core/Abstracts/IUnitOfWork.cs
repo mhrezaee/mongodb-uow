dc9dcbd6ab429acbbdf835934a364870b90231df
Hadi <se.hadireazee@gmail.com>
2023-08-27 12:19:22 +0200

    feat: implement unit of work

---
+++ MongoDbDotNet/src/MongoDbDotNet.Core/Abstracts/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using MongoDbDotNet.Core.Abstracts.Repositories;
+
+namespace MongoDbDotNet.Core.Abstracts;
+
+public interface IUnitOfWork
+{
+    IUserRepository Users { get; }
+    IChatRepository Chats { get; }
+
+    //add your real collection repository interfaces here.
+
+}
---
+++ MongoDbDotNet/src/MongoDbDotNet.Infrastructure/Concretes/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using MongoDbDotNet.Core.Abstracts.Repositories;
+using MongoDbDotNet.Core.Abstracts;
+using MongoDbDotNet.Infrastructure.Concretes.Repositories;
+
+
+namespace MongoDbDotNet.Infrastructure.Concretes;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly IMongoDatabase _database;
+
+    public UnitOfWork(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetConnectionString("Default"));
+        _database = client.GetDatabase(configuration.GetValue<string>("MongoOptions:DatabaseName"));
+
+    }
+
+    private IUserRepository _userRepository;
+    public IUserRepository Users => _userRepository ??= new UserRepository(_database);
+
+    private IChatRepository _chatRepository;
+    public IChatRepository Chats => _chatRepository ??= new ChatRepository(_database);
+
+}
