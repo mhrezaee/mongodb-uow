ca5ba635da15b811f843f838b36311da5c8a431a
Hadi <se.hadireazee@gmail.com>
2023-08-27 12:05:32 +0200

    feat: add entity repository interfaces with some sample specifications

---
+++ MongoDbDotNet/src/MongoDbDotNet.Core/Abstracts/Repositories/IChatRepository.cs
@@ -0,0 +1,9 @@
+﻿using MongoDbDotNet.Core.Entities;
+
+namespace MongoDbDotNet.Core.Abstracts.Repositories;
+
+public interface IChatRepository : IBaseRepository<Chat>
+{
+    Task<List<Chat>> GetAllByEmailAsync(string email, string version);
+    Task<List<Chat>> GetAllChatsAsync(string version);
+}
---
+++ MongoDbDotNet/src/MongoDbDotNet.Core/Abstracts/Repositories/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using MongoDbDotNet.Core.Entities;
+
+namespace MongoDbDotNet.Core.Abstracts.Repositories;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    Task<User> GetUserByEmailAsync(string emailAddress);
+}
